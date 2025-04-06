f359805ca25a6a738d023467356033ee9f7522be
Kevin Marroquin <kmarro4@uic.edu>
2025-04-06 01:45:20 -0500

    reset prompt

--- app.py
+++ app.py
@@ -123,5 +123,14 @@ def handle_user_input(data):
     print(response.text)
     emit("server-response", (response.text))
 
+@socketio.on("reset")
+def handle_reset(data):
+    global chat
+    print(data)
+    chat = client.chats.create(model=model)
+    response = chat.send_message(message=reset_prompt)
+    print(response.text)
+    emit("server-response", (response.text))
+
 if __name__ == '__main__':
     socketio.run(app)
