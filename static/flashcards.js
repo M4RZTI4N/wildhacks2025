9a89e31143aa5bdc076cda9f6862ff03494d88f9
M4RZTI4N <91927406+M4RZTI4N@users.noreply.github.com>
2025-04-06 10:47:36 -0500

    =

--- static/flashcards.js
+++ static/flashcards.js
@@ -2,7 +2,8 @@ var generate_flaschards = function(cards) {
 
 
     const flashcards = cards
-
+    console.log(flashcards)
+    flashcardContainer.style.display = "block"
     let currentIndex = 0;
     let isFlipped = false;
 
@@ -13,7 +14,9 @@ var generate_flaschards = function(cards) {
     const previousBtn = document.getElementById("prev-btn");
     const flipBtn = document.getElementById("flip-btn");
     const nextBtn = document.getElementById("next-btn");
-
+    generateBtn.onclick=()=>{
+        socket.emit("user-flashcards")
+    }
     function renderCard() {
         const card = flashcards[currentIndex];
         frontElement.textContent = card.front;
@@ -26,26 +29,24 @@ var generate_flaschards = function(cards) {
         flashcardElement.classList.toggle("flip");
         isFlipped = !isFlipped;
     }
-
+    currentIndex = 0;
+    renderCard();
+    // socket.emit("debug","TEST")
+    previousBtn.disabled = false;
+    nextBtn.disabled = false;
     flashcardElement.addEventListener("click", flipCard);
     const cardElements = document.getElementsByClassName("card");
     Array.from(cardElements).forEach(card => {
         card.addEventListener("click", flipCard);
     });
 
-    generateBtn.addEventListener("click", function() {
-        currentIndex = 0;
-        renderCard();
-        // socket.emit("debug","TEST")
-        previousBtn.disabled = false;
-        nextBtn.disabled = false;
-    });
+    
 
     previousBtn.addEventListener("click", function() {
         if (currentIndex > 0) {
             currentIndex--;
             renderCard();
-            previousBtn.disabled = true;
+            previousBtn.disabled = false;
         }
         if (currentIndex === 0) {
             previousBtn.disabled = true;
